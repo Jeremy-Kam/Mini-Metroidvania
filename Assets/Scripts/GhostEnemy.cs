29c0d13af7ca761f83b01be1d93abde91c15c3f3
Jijiaki <jijiyakiace@gmail.com>
2023-04-25 01:42:15 -0400

    Moved scripts, added ghost sprite, imported A*

---
+++ Assets/Scripts/GhostEnemy.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GhostEnemy : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    void FixedUpdate()
+    {
+        
+    }
+
+}
